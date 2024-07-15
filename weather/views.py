7b1db7baed4029e43dc3d83c1bcab9a63a4067bb
vlad <sapozhnikov.061098@mail.ru>
2024-07-15 15:30:03 +0300

    added data

--- weather/views.py
+++ weather/views.py
@@ -8,6 +8,12 @@ def weather(request):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api}'
 
     response = requests.get(url).json()
-    data = {'temp': response['main']['temp']}
+#    print(response)
+    data = {
+            'temp': response['main']['temp'],
+            'humidity': response['main']['humidity'],
+            'wind': response['wind']['speed'],
+            'deg': response['wind']['deg']
+            }
     context = {'data': data}
     return render(request, 'weather/main.html', context)
